81b9745c815d6490232fe12d1431e421f349c010
Daniel Sprague <danielsprague@Daniels-MacBook-Pro.local>
2019-01-01 13:33:03 -0500

    scan script

--- scan.py
+++ scan.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-fa',type=str)
+parser.add_argument('-q',type=str)
 parser.add_argument('-ref',type=str)
 parser.add_argument('-k',type=int)
 parser.add_argument('-w',default=1000,type=int)
@@ -20,6 +21,7 @@ fa = far.Reader(args.fa)
 headers,seqs = fa.get_headers(),fa.get_seqs()
 
 for i,seq in enumerate(seqs):
-    continue
-
-    
+    seqscan = SEEKRscanner(args.q,headers[i],seq,args.ref,args.k,args.w,args.s,args.thresh)
+    distributions = seqscan.querydist()
+    scan_df = seqscan.scan()
+    percentile = seq.scan()
